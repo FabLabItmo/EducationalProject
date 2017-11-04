aa2a983f84c44654f2aec635d633a9c6fb02316c
mlebedev <mlebdev@bcc.ru>
2017-11-04 13:27:19 +0300

    load image with Picasso

--- app/src/main/java/ru/ifmo/fablab/myapplication/MainActivity.java
+++ app/src/main/java/ru/ifmo/fablab/myapplication/MainActivity.java
@@ -1,12 +1,21 @@
 package ru.ifmo.fablab.myapplication;
 
+import android.databinding.BindingAdapter;
 import android.databinding.DataBindingUtil;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 import ru.ifmo.fablab.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindingAdapter({"app:url"})
+    public static void bindImageUrl(ImageView imageView, String url){
+        Picasso.with(imageView.getContext()).load(url).into(imageView);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
