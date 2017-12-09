15494079e74c4448ed52999d3eb45f76a40c7b5e
mlebedev <mlebdev@bcc.ru>
2017-12-09 14:07:19 +0300

    lesson 7

--- app/src/main/java/ru/ifmo/fablab/myapplication/MainActivity.java
+++ app/src/main/java/ru/ifmo/fablab/myapplication/MainActivity.java
@@ -1,13 +1,11 @@
 package ru.ifmo.fablab.myapplication;
 
 import android.databinding.BindingAdapter;
+import android.databinding.BindingConversion;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -15,11 +13,16 @@ import com.squareup.picasso.Picasso;
 import ru.ifmo.fablab.myapplication.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    @BindingConversion
+    public static int convertBooleanToVisibility(boolean b){
+        return b ? View.VISIBLE : View.INVISIBLE;
+    }
 
     @BindingAdapter({"app:url"})
     public static void bindImageUrl(ImageView imageView, String url){
         Picasso.with(imageView.getContext()).load(url).into(imageView);
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +30,5 @@ public class MainActivity extends AppCompatActivity {
         ActivityMainBinding binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
         final ViewModel vm = new ViewModel();
         binding.setVm(vm);
-        RecyclerView rv = findViewById(R.id.recycler_view);
-        rv.setLayoutManager(new LinearLayoutManager(this));
-        rv.setAdapter(new RecyclerView.Adapter<VH>() {
-            @Override
-            public VH onCreateViewHolder(ViewGroup parent, int viewType) {
-                return new VH(getLayoutInflater().inflate(R.layout.item_layout,parent,false));
-            }
-
-            @Override
-            public void onBindViewHolder(VH holder, int position) {
-                bindImageUrl(holder.imageView,vm.models.get(position).url);
-            }
-
-            @Override
-            public int getItemCount() {
-                return vm.models.size();
-            }
-        });
     }
-
-    class VH extends RecyclerView.ViewHolder{
-
-        ImageView imageView;
-
-        public VH(View itemView) {
-            super(itemView);
-            imageView = itemView.findViewById(R.id.image);
-        }
-    }
-
 }
--- app/src/main/java/ru/ifmo/fablab/myapplication/ViewModel.java
+++ app/src/main/java/ru/ifmo/fablab/myapplication/ViewModel.java
@@ -1,6 +1,8 @@
 package ru.ifmo.fablab.myapplication;
 
-import java.util.List;
+import android.databinding.ObservableArrayList;
+
+import me.tatarka.bindingcollectionadapter2.ItemBinding;
 
 /**
  * Created by mlebedev on 04/11/2017.
@@ -8,6 +10,17 @@ import java.util.List;
 
 public class ViewModel {
 
-    public List<ItemModel> models = Model.getInstance().items;
+    public ObservableArrayList<ItemViewModel> models = new ObservableArrayList<>();
 
+    public ItemBinding<ItemViewModel> itemBinding = ItemBinding.of(BR.item,R.layout.item_layout);
+
+    public ViewModel() {
+        for(ItemModel itemModel : Model.getInstance().items){
+            models.add(new ItemViewModel(itemModel,this));
+        }
+    }
+
+    public void delete(ItemViewModel item){
+        models.remove(item);
+    }
 }
